6b6a184143b860b2fb0c49a7cb6ba1c2f93bed6a
Jacob MacElroy <30531294+jmacelroy@users.noreply.github.com>
2021-05-25 16:26:13 -0500

    Adding divert to manifest.

--- health-checker/handler.go
+++ health-checker/handler.go
@@ -16,45 +16,6 @@ type ServiceHealth struct {
 	HealthResults []HealthResult `json:"health_results"`
 }
 
-// var results = []ServiceHealth{
-// 	{
-// 		ServiceID: "3d8201b4-d152-4d84-b8bc-eb87817a617e",
-// 		HealthResults: []HealthResult{
-// 			{
-// 				Healthy:   true,
-// 				Timestamp: "1621778498",
-// 			},
-// 		},
-// 	},
-// 	{
-// 		ServiceID: "a41a7cb2-6ab1-489d-9dab-ff008e4ce34e",
-// 		HealthResults: []HealthResult{
-// 			{
-// 				Healthy:   false,
-// 				Timestamp: "1621778553",
-// 			},
-// 		},
-// 	},
-// 	{
-// 		ServiceID: "028d4690-f9b8-4cb2-9edb-a44d831dbed3",
-// 		HealthResults: []HealthResult{
-// 			{
-// 				Healthy:   true,
-// 				Timestamp: "1621778592",
-// 			},
-// 		},
-// 	},
-// 	{
-// 		ServiceID: "d2647c9b-2e15-4978-a97f-2bcad9126f57",
-// 		HealthResults: []HealthResult{
-// 			{
-// 				Healthy:   true,
-// 				Timestamp: "1621778628",
-// 			},
-// 		},
-// 	},
-// }
-
 type HealthClient interface {
 	Get() []ServiceHealth
 }
