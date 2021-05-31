e83b320a06986b94e5e0d54f0c6104ee97976a78
Ramon Lamana <rlamana@gmail.com>
2021-05-31 17:46:21 +0200

    App layout

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { blue } from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import Catalog from './containers/Catalog';
 import './app.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+    },
+    secondary: {
+      main: '#11cb5f',
+    }
+  }
+});
+
 const useStyles = makeStyles(theme => ({
-  header: {
-    margin: '12px 24px'
+  root: {
+    flexGrow: 1,
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+
+  title: {
+    flexGrow: 1,
   }
 }));
 
@@ -16,12 +43,22 @@ function App() {
   const classes = useStyles();
 
   return (
-    <>
-      <Typography variant="h5" gutterBottom className={classes.header}>
-        Service Catalog
-      </Typography>
-      <Catalog />
-    </>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.title}>
+              Service Catalog
+            </Typography>
+            <Button color="inherit">Refresh</Button>
+          </Toolbar>
+        </AppBar>
+        <Catalog />
+      </div>
+    </ThemeProvider>
   );
 }
 
